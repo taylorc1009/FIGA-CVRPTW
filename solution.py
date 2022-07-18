7f76d35820e525e1ecd72ae2e19fd3c181d62928
Taylor <taylorcourtney2000@googlemail.com>
2022-07-18 11:52:12 +0100

    Optimised error checker

--- solution.py
+++ solution.py
@@ -26,6 +26,7 @@ class Solution:
             self.vehicles[i].calculate_length_of_route(instance)
 
     def check_format_is_correct(self, instance: ProblemInstance) -> None:
+        node_nums = set(range(1, 101))
         # error checks to ensure that every route is of the valid format
         if sum(v.get_num_of_customers_visited() for v in self.vehicles) != len(instance.nodes) - 1: # check if the solution contains the correct amount of destinations, in that it visits all of them
             raise ValueError(f"Mismatched amount of destinations: {sum(v.get_num_of_customers_visited() for v in self.vehicles)}")
@@ -33,5 +34,5 @@ class Solution:
             raise ValueError("Number of destinations was not at least 2")
         elif [v for v in self.vehicles if v.destinations[0].node.number or v.destinations[-1].node.number]: # checks that every route starts and ends at the depot
             raise ValueError("Indexes 0 and n - 1 should be depot nodes")
-        elif [set(range(1, 101)).remove(d.node.number) for v in self.vehicles for d in v.get_customers_visited()][0]: # checks if all nodes have been visited; ".remove" will also find both: duplicate nodes as it will throw an exception when it tries to remove an already-removed node, and depot nodes in the middle of a route as the set starts at 1, so if it tries to remove the depot (node 0) it won't exist and throw an exception
+        elif node_nums.difference(set(d.node.number for v in self.vehicles for d in v.get_customers_visited())): # checks if all nodes have been visited; ".remove" will also find both: duplicate nodes as it will throw an exception when it tries to remove an already-removed node, and depot nodes in the middle of a route as the set starts at 1, so if it tries to remove the depot (node 0) it won't exist and throw an exception
             raise ValueError("Not all nodes are visited")
